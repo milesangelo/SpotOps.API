d908afd07b0922367860279490dcd61b4c73ab64
milesangelo <milesvendetti@gmail.com>
2021-12-30 08:50:37 -0700

    Adds SpotOps Tests project with add tests on spot controller

---
+++ SpotOps.Test/Api/Controllers/SpotControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SpotOps.Api.Controllers;
+using SpotOps.Api.Models.ResponseModels;
+using SpotOps.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpotOps.Test.Api.Controllers
+{
+    [TestFixture]
+    internal class SpotControllerTests
+    {
+        [Test]
+        public void Post_When_CalledWithValidFormData_Should_ReturnOkObjectResult()
+        {
+            var spotController = new SpotsController(new MockSpotResponseService());
+
+            var spotResponse = new SpotResponse() 
+            { 
+                Name = "test",
+                DateCreated = DateTime.Now,
+                Id = 1,
+                Type = "testtype"
+            };
+
+            var actionResult = spotController.Post(spotResponse).Result;
+            OkObjectResult? okResult = actionResult as OkObjectResult;
+
+            Assert.IsNotNull(actionResult);
+            Assert.AreEqual(200, okResult?.StatusCode);
+        }
+    }
+}
