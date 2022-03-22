cb6288ce74787671d9c6fc82a0bb82f7236aeaa1
milesangelo <milesvendetti@gmail.com>
2022-03-22 13:19:35 -0600

    adds unsecure ping endpoint

---
+++ Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpotOps.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PingController: ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "pong";
+    }
+}
---
+++ SpotOps.Test/Api/Controllers/PingControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SpotOps.Api.Controllers;
+
+namespace SpotOps.Test.Api.Controllers;
+
+[TestFixture]
+public class PingControllerTests
+{
+    [Test]
+    public void Get_When_Pinged_Should_ReturnPonged()
+    {
+        var pingController = new PingController();
+
+        var message = pingController.Get();
+        
+        Assert.AreEqual(message, "pong");
+    }
+}
